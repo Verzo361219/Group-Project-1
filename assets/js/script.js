7f4584566e6d8fced8a02707506b0c335feb0e5f
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-08-15 21:49:35 -0400

    troubleshoot #5

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ var settings2 = {
     "url": "https://api.kroger.com/v1/products?filter.brand=Kroger&filter.term=chicken&filter.locationId=01400943&filter.limit=1",
     "method": "GET",
     "headers": {
-      "Access-Control-Allow-Origin": https://verzo361219.github.io/Group-Project-1/
+      "Access-Control-Allow-Origin": "https://verzo361219.github.io/Group-Project-1/",
       "Accept": "application/json",
       "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJ3aGF0c2ZvcmRpbm5lcnRvbmlnaHQtZGIwZDliMzQ2MmYzYmM5ZWRmZWYwNjkyNDIxMThlMDU0NzA0ODE2MDgyMTA2NTgxNDMxIiwiZXhwIjoxNjYwNjE1NjI1LCJpYXQiOjE2NjA2MTM4MjAsImlzcyI6ImFwaS5rcm9nZXIuY29tIiwic3ViIjoiYTMwNmM3NGItYzM1ZC01MjNmLWFmNDgtN2U5YWYyMzE1MGIwIiwic2NvcGUiOiJwcm9kdWN0LmNvbXBhY3QiLCJhdXRoQXQiOjE2NjA2MTM4MjUxNTc2NTY2ODEsImF6cCI6IndoYXRzZm9yZGlubmVydG9uaWdodC1kYjBkOWIzNDYyZjNiYzllZGZlZjA2OTI0MjExOGUwNTQ3MDQ4MTYwODIxMDY1ODE0MzEifQ.q4e6Ny--XoMl-ZkL-72CI9MlYOR-rL_qOth4XK05ijH6SmWqLDOyIcQa-J9EmLnPH3AG83VFp3_TfFEGIZMJUMoL9HqayfBZVOXzEHaP_N1Mc6qphcTryotciM2uIE2Gvy7x1vBuRgoLQ1pBOjhYBc72DPLqbgDpHWGqLofxxEB_kANli_FF6TsazrfYLJdtWxTdrQOEVcFeuAHSJox4ZuRrao_htJ5VR11lNXAYRfJYNuyw9-neIZnxg6HTBPDvsRNQMWAjkX-OIJ3kaJ5-Y1EpRgegpDxy1UOq_zRLAeYiMgcmwsRMf6U3k1rhSYKa6xnDN3iSdOBEdtm1B6Ceig"
     }
