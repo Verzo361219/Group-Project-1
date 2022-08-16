efa2f691db25e3cec2ba28ba03a49f8b8ea98532
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-08-15 21:16:27 -0400

    api now pulls the specific price of items searched as well as displays the price in the shopping list section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://cors-anywhere.herokuapp.com/https://api.kroger.com/v1/connect/oauth2/token",
-    "method": "POST",
-    "headers": {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Authorization": "Basic d2hhdHNmb3JkaW5uZXJ0b25pZ2h0LWRiMGQ5YjM0NjJmM2JjOWVkZmVmMDY5MjQyMTE4ZTA1NDcwNDgxNjA4MjEwNjU4MTQzMTpjN0xkNnB2WFF3alNJa0ZvMVQ4aTdOR3NWcm1oaWVGbUhRRzJkck90"
-    },
-    "data": {
-      "grant_type": "client_credentials",
-      "scope": "product.compact"
-    }
-  }
+// var settings = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": "https://cors-anywhere.herokuapp.com/https://api.kroger.com/v1/connect/oauth2/token",
+//     "method": "POST",
+//     "headers": {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//       "Authorization": "Basic d2hhdHNmb3JkaW5uZXJ0b25pZ2h0LWRiMGQ5YjM0NjJmM2JjOWVkZmVmMDY5MjQyMTE4ZTA1NDcwNDgxNjA4MjEwNjU4MTQzMTpjN0xkNnB2WFF3alNJa0ZvMVQ4aTdOR3NWcm1oaWVGbUhRRzJkck90"
+//     },
+//     "data": {
+//       "grant_type": "client_credentials",
+//       "scope": "product.compact"
+//     }
+//   }
 
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
+//   $.ajax(settings).done(function (response) {
+//     console.log(response);
+//   });
 
 
 
-var shoppinglist = document.getElementById("#shoppinglist")
+
+var list = $("#shoppingList")
+console.log(list)
 var settings2 = {
     "async": true,
     "crossDomain": true,
@@ -27,7 +29,7 @@ var settings2 = {
     "method": "GET",
     "headers": {
       "Accept": "application/json",
-      "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJ3aGF0c2ZvcmRpbm5lcnRvbmlnaHQtZGIwZDliMzQ2MmYzYmM5ZWRmZWYwNjkyNDIxMThlMDU0NzA0ODE2MDgyMTA2NTgxNDMxIiwiZXhwIjoxNjYwNjA5MTY5LCJpYXQiOjE2NjA2MDczNjQsImlzcyI6ImFwaS5rcm9nZXIuY29tIiwic3ViIjoiYTMwNmM3NGItYzM1ZC01MjNmLWFmNDgtN2U5YWYyMzE1MGIwIiwic2NvcGUiOiJwcm9kdWN0LmNvbXBhY3QiLCJhdXRoQXQiOjE2NjA2MDczNjkzMjk2MzI2MzgsImF6cCI6IndoYXRzZm9yZGlubmVydG9uaWdodC1kYjBkOWIzNDYyZjNiYzllZGZlZjA2OTI0MjExOGUwNTQ3MDQ4MTYwODIxMDY1ODE0MzEifQ.v62zmjFF3jeCiZ05jP73CJmVVA2JIuR3ASkJlitVYpkO4ahnllvnqsCeQb9GqXaenKoPQza1jimK-SpGvHIgXrXxz4aTOC-XS4ulbZL6o-RSeCx_Z7xybTs6VKl7KJB_-wjUsEeWMPfr5JYP0KTsJ6yBVKgpVWdJMeljVNhbdnUH-FSH_8MCxNhBHLIrN8IQG8aHNIzaTCazKWGyUNc06blmKhAGhDczD_4IFk6cNoBL8vY8XU7AcP7INAWzLgHSpL8wtkWhnRPpgD870NjXKUykSlLG37L3vYrEa6M0YM1_vw89aCmyJRoFyCm-SIfQ1zM-_laXiknK_VXumZpYsQ"
+      "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJ3aGF0c2ZvcmRpbm5lcnRvbmlnaHQtZGIwZDliMzQ2MmYzYmM5ZWRmZWYwNjkyNDIxMThlMDU0NzA0ODE2MDgyMTA2NTgxNDMxIiwiZXhwIjoxNjYwNjEyODQ3LCJpYXQiOjE2NjA2MTEwNDIsImlzcyI6ImFwaS5rcm9nZXIuY29tIiwic3ViIjoiYTMwNmM3NGItYzM1ZC01MjNmLWFmNDgtN2U5YWYyMzE1MGIwIiwic2NvcGUiOiJwcm9kdWN0LmNvbXBhY3QiLCJhdXRoQXQiOjE2NjA2MTEwNDc4NzA1MTA4NzksImF6cCI6IndoYXRzZm9yZGlubmVydG9uaWdodC1kYjBkOWIzNDYyZjNiYzllZGZlZjA2OTI0MjExOGUwNTQ3MDQ4MTYwODIxMDY1ODE0MzEifQ.N-xkZQdh1kZQkuVnJF3Jb7qBBrhPFar6DGIVGzNsE-XYoh4tSwlLZQ2Yb2liD1OsoLBju_iF2zlQsUXQeYIDSdPSZVbNMBDoZFiXikCtTGU8fOhvezMvKLLHZ97K7IYsexaNHuXk4U85xXNZxt82Azq-Z2JEDNoZHKRcq3fDjmtkcj166RNQh97tmiybi7j1LyqtJwc_8gCSdelWy1JQoM0fF3NXEtXDJIL0GSBEC5Z1qDoyHK5xBWaMWOH5dFMbb83IhKEywTQRZ-y4hMrqrQhOxkh42UJxM_IaNxi8kEWFRJYowEctZAPAyWDeW8wGZCD16r-5bC8h_Tnm-8p30Q"
     }
   }
   
@@ -37,15 +39,11 @@ var settings2 = {
   .then(function(data2){
     var price = data2.data[0].items[0].price.regular
     console.log(price)
-   
-  })
-  .then(function(displayprices){
-    var list = document.createElement('ul');
-    shoppinglist.appendChild(list)
-
+    console.log(list)
     var item = document.createElement('li')
-    list.appendChild(item)
-
-    item.textContent(price);
-
+    item.textContent = "$ " + price
+    console.log(item)
+    $(item).appendTo(list)
+    
   })
+ 
