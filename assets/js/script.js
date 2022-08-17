cd5dd403fd147cd510737012635e76231e70fca4
CBruno <christian.bruno1995@gmail.com>
2022-08-17 17:28:04 -0400

    Added javascript to create a modal for the recipe cards for each meal. Updated stylesheet to give it formatting and rearranged script tags at the bottom of the html so that jquery can work. Still need to get the modal to stay open until its dismissed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var cardContainer = document.querySelector('.cardContainer');
 var searchInput = document.querySelector('#searchMealInput');
 var searchBtn =  document.querySelector('#btnSearch');
+var ingredientsList = document.querySelector('.ingredients-list')
+var modalContainer = document.querySelector('.modalContainer')
 
 searchBtn.addEventListener("click", handleMealFetch);
 
@@ -28,6 +30,7 @@ var requestUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=' + mealS
             card.classList.add("card","col", "s12", "m6", "l4");
             card.style.marginLeft ="5px";
             card.style.width ="32.33%";
+            card.setAttribute("dataid", data.meals[i].idMeal)
 
             var cardImage = document.createElement('div');
             card.appendChild(cardImage);
@@ -51,7 +54,9 @@ var requestUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=' + mealS
             mealName.textContent = data.meals[i].strMeal
 
             var recipeBtn = document.createElement("a")
-            recipeBtn.classList.add("btn-floating", "btn-medium", "waves-effect", "waves-light", "red","btn-margin");
+            recipeBtn.classList.add("recipeBtn","btn-floating", "btn-medium", "waves-effect", "waves-light", "red", "btn-margin", "modal-trigger");
+            recipeBtn.href = "#modal1"
+            recipeBtn.addEventListener("click", getMealRecipe);
             $(cardContent).append(recipeBtn);
 
             var addIcon = document.createElement("i");
@@ -67,7 +72,64 @@ var requestUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=' + mealS
             addIcon.classList.add("material-icons");
             addIcon.innerHTML = "add";
             $(addBtn).append(addIcon);
+            }
+        });
 
+function getMealRecipe(meal){
+    meal.preventDefault();
+    console.log("button Pushed");
+    if(meal.currentTarget.classList.contains('modal-trigger')){
+        console.log("true");
+        var mealItem = meal.currentTarget.parentElement.parentElement;
+        console.log(mealItem.getAttribute("dataid"));
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.getAttribute("dataid")}`)
+        .then (function(response) {
+            return response.json()
+        })
+        .then (function(data) {
+            mealRecipeModal(data.meals);
+        })
+    }
+}
+
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal = meal[0];
+    var modalHTML = `<div id="modal1" class="modal modal-fixed-footer">
+    <div class="modal-content">
+        <h4 class="recipe-title">${meal.strMeal}</h4>
+        <div class="recipe-meal-img">
+            <img src="${meal.strMealThumb}" alt = "${meal.strMeal}">
+        </div>
+        <p class="recipe-category">${meal.strCategory}</p>
+        <div class="recipe-instructions">
+            <h5>Instructions:</h5>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class="recipe-ingredients">
+            <h5>Ingredients:</h5>
+            <ul class="ingredients-list">
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+            </ul>
+        </div>
+        <div class="recipe-link">
+            <a href="${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-action modal-close waves-effect waves-red red lighten-2 btn"><i class="material-icons left">close</i>close</a>
+        <a href="#!" class="modal-action modal-close waves-effect waves-light btn indigo"><i class="material-icons left">add_box</i>Add To List</a>
+    </div>
+</div>`;
+    $(modalContainer).html(modalHTML);
+    $(document).ready(function(){
+        $('.modal').modal();
+    });
+}}
 
             // var mealName = document.createElement('h4');
             // mealName.classList.add("card-title")
@@ -79,17 +141,17 @@ var requestUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=' + mealS
 
             // mealName.textContent = data.meals[0].strMeal;
 
-            var mealID = data.meals[0].idMeal;
-            console.log(mealID);
+            // var mealID = data.meals[0].idMeal;
+            // console.log(mealID);
 
-            var requestMealURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealID;
-            fetch(requestMealURL)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then (function (data1) {
-                    console.log(data.meals[0].strMeal);
-                    console.log(data.meals[0].strMealThumb);
+            // var requestMealURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealID;
+            // fetch(requestMealURL)
+            //     .then(function (response) {
+            //         return response.json();
+            //     })
+            //     .then (function (data1) {
+            //         console.log(data.meals[0].strMeal);
+            //         console.log(data.meals[0].strMealThumb);
                     // var instructionsList = document.createElement('li');
                     // instructionsList.classList.add("card-text");
                     // cardBody.appendChild(instructionsList);
@@ -118,6 +180,4 @@ var requestUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=' + mealS
                                             
         //                 console.log(measurements);
         //                 console.log(ingredients);
-                })
-}})
-        };
+                
